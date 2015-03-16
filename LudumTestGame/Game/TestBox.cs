5ed18e436e6997bb899db6652297d1df8b926f79
Linus Närkling <VapidLinus@gmail.com>
2015-03-16 12:45:00 +0100

    Adpated to engine changes

--- LudumTestGame/Game/TestBox.cs
+++ LudumTestGame/Game/TestBox.cs
@@ -16,29 +16,29 @@ namespace TestGame
 		{
 			collider = GameObject.AddComponent<BoxCollider>();
 			renderer = GameObject.AddComponent<RectangleOutlineRenderer>();
-			renderer.Size = collider.Size = new Vector2(1, 1);
+			Transform.Scale = new Vector2(1, 1);
 		}
 
 		public override void OnStart()
 		{
 			if (canMove)
-				renderer.Size = collider.Size = new Vector2(.6, 2);
+				Transform.Scale = new Vector2(.6, 2);
 		}
 
 		public override void OnFixedUpdate()
 		{
 			if (!canMove) return;
 
-			Transform.Position = new Vector2(0, -collider.Size.y + .5);
+			Transform.Position = new Vector2(0, -Transform.Scale.y + .5);
 			
 			if (Input.IsKeyDown(Keyboard.Key.D))
-				Transform.Position += Vector2.Right * collider.Size.x;
+				Transform.Position += Vector2.Right * Transform.Scale.x;
 			if (Input.IsKeyDown(Keyboard.Key.A))
-				Transform.Position += Vector2.Left * collider.Size.x;
+				Transform.Position += Vector2.Left * Transform.Scale.x;
 			if (Input.IsKeyDown(Keyboard.Key.W))
-				Transform.Position += Vector2.Up * collider.Size.y;
+				Transform.Position += Vector2.Up * Transform.Scale.y;
 			if (Input.IsKeyDown(Keyboard.Key.S))
-				Transform.Position += Vector2.Down * collider.Size.y;
+				Transform.Position += Vector2.Down * Transform.Scale.y;
 		}
 
 		public override void OnUpdate()
